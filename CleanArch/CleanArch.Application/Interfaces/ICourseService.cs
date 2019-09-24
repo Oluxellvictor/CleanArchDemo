db09990b96d2e59b8617f3bd1441851ae0a0086f
oluxell.voa007@gmail.com <oluxell.voa007@gmail.com>
2019-09-23 19:33:34 -0700

    Commit Services and Repository Interfaces,  CourseViewModels

---
+++ CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using CleanArch.Application.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Interfaces
+{
+    interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
